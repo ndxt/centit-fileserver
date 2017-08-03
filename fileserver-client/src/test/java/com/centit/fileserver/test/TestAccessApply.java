122c470c0d636fec792bbae972e326ba8923e021
杨淮生 <codefan@centit.com>
2017-08-03 17:03:29 +0800

    测试 RequestBody FastJSON

--- fileserver-client/src/test/java/com/centit/fileserver/test/TestAccessApply.java
+++ fileserver-client/src/test/java/com/centit/fileserver/test/TestAccessApply.java
@@ -2,16 +2,31 @@ package com.centit.fileserver.test;
 
 import com.centit.fileserver.client.FileClientImpl;
 import com.centit.fileserver.client.po.FileAccessLog;
+import com.centit.framework.core.common.ResponseJSON;
+import com.centit.support.network.HttpExecutor;
+import org.apache.http.impl.client.CloseableHttpClient;
 
 /**
  * Created by codefan on 17-8-3.
  */
 public class TestAccessApply {
     public static void main(String[] args) throws Exception {
-        FileClientImpl fileClient = new FileClientImpl();
-        fileClient.init("http://codefanbook:8080/fileserver",
-                "u0000000","000000","http://codefanbook:8080/fileserver");
+
         FileAccessLog accessLog = new FileAccessLog();
-        System.out.println(fileClient.getFileUrl(accessLog));
+        accessLog.setAccessRight("T");
+        accessLog.setAccessTimes(5);
+        accessLog.setAccessUsename("管理员");
+        accessLog.setAccessUsercode("u0000000");
+        accessLog.setFileId("File0000000000000000001");
+
+        try(CloseableHttpClient httpClient = HttpExecutor.createHttpClient()) {
+            String jsonStr = HttpExecutor.formPost(httpClient,
+                    "http://localhost:8080/fileserver/service/access/apply", accessLog);
+            System.out.println(jsonStr);
+
+            jsonStr = HttpExecutor.jsonPost(httpClient,
+                    "http://localhost:8080/fileserver/service/access/japply", accessLog);
+            System.out.println(jsonStr);
+        }
     }
 }
