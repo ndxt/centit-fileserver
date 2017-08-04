18c50dea287e440ae61d76db47b8f0055971c2bb
zou_wy <zou_wy@centit.com>
2017-08-04 15:02:20 +0800

    修改 配置文件

--- fileserver-web/src/main/java/com/centit/fileserver/config/ServiceConfig.java
+++ fileserver-web/src/main/java/com/centit/fileserver/config/ServiceConfig.java
@@ -3,12 +3,14 @@ package com.centit.fileserver.config;
 import com.centit.fileserver.listener.InstantiationServiceBeanPostProcessor;
 import com.centit.framework.components.impl.NotificationCenterImpl;
 import com.centit.framework.components.impl.TextOperationLogWriterImpl;
+import com.centit.framework.config.H2Config;
+import com.centit.framework.config.RedisConfig;
 import com.centit.framework.hibernate.config.DataSourceConfig;
 import com.centit.framework.ip.app.config.IPAppSystemBeanConfig;
-import com.centit.framework.ip.service.IntegrationEnvironment;
-import com.centit.framework.ip.service.impl.DummyIntegrationEnvironment;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
+import com.centit.framework.staticsystem.config.SpringSecurityCasConfig;
+import com.centit.framework.staticsystem.config.SpringSecurityDaoConfig;
 import org.springframework.context.annotation.*;
 
 /**
@@ -16,16 +18,19 @@ import org.springframework.context.annotation.*;
  */
 @ComponentScan(basePackages = "com.centit",
         excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
-@Import({IPAppSystemBeanConfig.class, DataSourceConfig.class})
-@Configuration
+@Import({RedisConfig.class,
+        H2Config.class,
+        SpringSecurityDaoConfig.class,
+        SpringSecurityCasConfig.class,
+        IPAppSystemBeanConfig.class,
+        DataSourceConfig.class})
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ServiceConfig {
-
-
-    @Bean
+   /* @Bean
     @Lazy(value = false)
     public IntegrationEnvironment integrationEnvironment() {
         return new DummyIntegrationEnvironment();
-    }
+    }*/
 
     @Bean
     public NotificationCenter notificationCenter() {
