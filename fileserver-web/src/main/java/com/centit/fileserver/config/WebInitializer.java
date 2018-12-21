7b4623204683f3bccce88e1df77e2bf1c2507cff
杨淮生 <codefan@centit.com>
2018-12-21 15:02:55 +0800

    添加 servlet 名称的设置

--- fileserver-web/src/main/java/com/centit/fileserver/config/WebInitializer.java
+++ fileserver-web/src/main/java/com/centit/fileserver/config/WebInitializer.java
@@ -1,5 +1,6 @@
 package com.centit.fileserver.config;
 
+import com.centit.framework.common.SysParametersUtils;
 import com.centit.framework.config.SystemSpringMvcConfig;
 import com.centit.framework.config.WebConfig;
 import com.centit.support.file.PropertiesReader;
@@ -74,7 +75,9 @@ public class WebInitializer implements WebApplicationInitializer {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(NormalSpringMvcConfig.class);
         ServletRegistration.Dynamic system  = servletContext.addServlet("service", new DispatcherServlet(context));
-        system.addMapping("/service/*");
+        String servletName = SysParametersUtils.getStringValue("app.servlet.name","service");
+        system.addMapping("/"+ servletName+"/*");
+        //system.addMapping("/service/*");
         system.setLoadOnStartup(1);
         system.setAsyncSupported(true);
     }
