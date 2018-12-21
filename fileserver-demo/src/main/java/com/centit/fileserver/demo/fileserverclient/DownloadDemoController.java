e1b81000749cd00fdb7b56401ed81571a3852d32
zou_wy <zou_wy@centit.com>
2018-12-21 15:29:10 +0800

    修改配置

--- fileserver-demo/src/main/java/com/centit/fileserver/demo/fileserverclient/DownloadDemoController.java
+++ fileserver-demo/src/main/java/com/centit/fileserver/demo/fileserverclient/DownloadDemoController.java
@@ -2,18 +2,22 @@ package com.centit.fileserver.demo.fileserverclient;
 
 import com.centit.fileserver.client.FileClient;
 import com.centit.framework.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@Controller
-//@RequestMapping("/demo")
+@Controller
+@RequestMapping("/demo")
 public class DownloadDemoController extends BaseController {
 
     //private static final Logger logger = LoggerFactory.getLogger(DownLoadController.class);
 
-    //@Resource
+    @Resource
     protected FileClient fileClient;
 
     /**
@@ -25,7 +29,7 @@ public class DownloadDemoController extends BaseController {
      * @return String
      * @throws Exception Exception
      */
-    //@RequestMapping(value = "/download/{attachId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/download/{attachId}", method = RequestMethod.GET)
     public String downloadFile(@PathVariable("attachId") String attachId, HttpServletRequest request,
                                HttpServletResponse response) throws Exception {
         String fileId = attachId;//根据 attachId获得 对应的fileID
