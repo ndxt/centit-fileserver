b1e9a2abbfe1b48a2b9536f5652a540805edec68
huzhifei <huzhifeiatnj@163.com>
2019-04-25 15:06:48 +0800

    加入dockerfile相关的文件

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #--------------------------------------------
-#鍙傛暟璇存槑:sh build-deploy.sh ${DEPLOYMENT} ${MODULE}
-#鏈緥鎵ц:sh build-deploy.sh fileserver fileserver-web
+#参数说明:sh build-deploy.sh ${DEPLOYMENT} ${MODULE}
+#本例执行:sh build-deploy.sh fileserver fileserver-web
 #--------------------------------------------
 DEPLOYMENT=$1
 MOUDLE=$2
 TIME=$(date +%Y%m%d%H%M)
 GIT_REVISION=$(git log -1 --pretty=format:"%h")
 IMAGE_NAME=172.29.0.13:8082/${DEPLOYMENT}:${TIME}_${GIT_REVISION}
-########缂栬瘧########
+########编译########
 mvn -U -pl ${MOUDLE} -am clean package -DskipTests=true
-########鍐欏叆dockerfile鏂囦欢########
+########写入dockerfile文件########
 cat >./${MOUDLE}/Dockerfile <<EOF
 FROM tomcat
 MAINTAINER hzf "hzf@centit.com"
@@ -18,18 +18,18 @@ ADD target/*.war /usr/local/tomcat/webapps/${DEPLOYMENT}.war
 EXPOSE 8080
 CMD /usr/local/tomcat/bin/startup.sh && tail -f /usr/local/tomcat/logs/catalina.out
 EOF
-########鏋勫缓闀滃儚########
+########构建镜像########
 cd ./${MOUDLE}/
 docker build -t ${IMAGE_NAME} .
-########涓婁紶nexus########
+########上传nexus########
 docker login -u developer -p centit 172.29.0.13:8082
 docker push ${IMAGE_NAME}
-########鍒犻櫎瀹瑰櫒########
+########删除容器########
 docker rm -f ${DEPLOYMENT} |true
-########鍒犻櫎闀滃儚########
+########删除镜像########
 docker image rm ${IMAGE_NAME} |true
 docker images|grep none|awk '{print $3}'|xargs docker rmi |true
-########杩炴帴nexus绉佹湇########
+########连接nexus私服########
 docker login -u developer -p centit 172.29.0.13:8082
-########杩愯########
+########运行########
 docker run -d -p 14000:8080 --name ${DEPLOYMENT} ${IMAGE_NAME}
