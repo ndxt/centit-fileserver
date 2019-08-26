4a2573f9e9306da4cc5289571b203d6dae41c9e2
zhangbin <zhangbin@centit.com>
2019-08-26 17:36:37 +0800

    看看能不能提交

--- fileserver-store-cos-plugin/src/test/java/com/centit/fileserver/store/plugin/test/CosTest.java
+++ fileserver-store-cos-plugin/src/test/java/com/centit/fileserver/store/plugin/test/CosTest.java
@@ -7,6 +7,8 @@ import com.qcloud.cos.http.HttpMethodName;
 import com.qcloud.cos.model.GeneratePresignedUrlRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import com.qcloud.cos.region.Region;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.net.URL;
@@ -15,22 +17,30 @@ import java.util.Date;
 public class CosTest {
     private static String bucketName = "filesave-1259276167";
     private static String key = "TestFile.txt";
+    private static COSClient client;
+
+    static {
+        String accessKey = "AKID30Qk1EkhzNells9lhzlgV8lJgQfrpr01";
+        String secretKey = "98ZIwTAi8gBEi6d3216StvZGt5kWUmS1";
+        BasicCOSCredentials credentials = new BasicCOSCredentials(accessKey, secretKey);
+        String regionName = "ap-shanghai";
+        ClientConfig config = new ClientConfig(new Region(regionName));
+        client = new COSClient(credentials, config);
+    }
 
     public static void main(String[] args) {
-        testDownloadUrl();
+//        testDownloadUrl();
+//        System.out.println(FileUtils.getTempDirectoryPath());
+        testDelete();
     }
 
     public static void testPut() {
-        COSClient cosClient = getCosClient();
-
-        PutObjectResult result = cosClient.putObject(bucketName, key, new File("D:\\TestFile.txt"));
+        PutObjectResult result = client.putObject(bucketName, key, new File("D:\\wa.pdf"));
         System.out.println(result.getExpirationTime());
     }
 
     public static void testDelete() {
-
-        COSClient cosClient = getCosClient();
-        cosClient.deleteObject(bucketName, key);
+        client.deleteObject(bucketName, key);
     }
 
     public static void testDownloadUrl() {
@@ -41,18 +51,17 @@ public class CosTest {
         // 设置签名在半小时后过期
         Date expirationDate = new Date(System.currentTimeMillis() + 30L * 60L * 1000L);
         request.setExpiration(expirationDate);
-        COSClient cosClient = getCosClient();
-        URL downloadUrl = cosClient.generatePresignedUrl(request);
+        URL downloadUrl = client.generatePresignedUrl(request);
         System.out.println(downloadUrl);
         // http://filesave-1259276167.cos.ap-shanghai.myqcloud.com/TestFile.txt?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKID30Qk1EkhzNells9lhzlgV8lJgQfrpr01%26q-sign-time%3D1558950548%3B1558952347%26q-key-time%3D1558950548%3B1558952347%26q-header-list%3D%26q-url-param-list%3D%26q-signature%3D19dd69768e3f71272366eafd389551ba88294b3d
     }
 
-    public static COSClient getCosClient() {
-        String accessKey = "AKID30Qk1EkhzNells9lhzlgV8lJgQfrpr01";
-        String secretKey = "98ZIwTAi8gBEi6d3216StvZGt5kWUmS1";
-        BasicCOSCredentials basicCOSCredentials = new BasicCOSCredentials(accessKey, secretKey);
-        String regionName = "ap-shanghai";
-        ClientConfig clientConfig = new ClientConfig(new Region(regionName));
-        return new COSClient(basicCOSCredentials, clientConfig);
-    }
+//    public static COSClient getCosClient() {
+//        String accessKey = "AKID30Qk1EkhzNells9lhzlgV8lJgQfrpr01";
+//        String secretKey = "98ZIwTAi8gBEi6d3216StvZGt5kWUmS1";
+//        BasicCOSCredentials credentials = new BasicCOSCredentials(accessKey, secretKey);
+//        String regionName = "ap-shanghai";
+//        ClientConfig config = new ClientConfig(new Region(regionName));
+//        return new COSClient(credentials, config);
+//    }
 }
