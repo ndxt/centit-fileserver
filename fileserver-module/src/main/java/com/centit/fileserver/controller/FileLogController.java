e05466f11dfc3d33f07148434232a500919c5747
zhf <zhf@centit.com>
2020-08-24 10:29:58 +0800

    修改

--- fileserver-module/src/main/java/com/centit/fileserver/controller/FileLogController.java
+++ fileserver-module/src/main/java/com/centit/fileserver/controller/FileLogController.java
@@ -9,7 +9,6 @@ import com.centit.support.algorithm.DatetimeOpt;
 import com.centit.support.database.utils.PageDesc;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,19 +35,20 @@ public class FileLogController extends BaseController {
     @ApiOperation(value = "新增文件日志")
     @WrapUpResponseBody
     public void createFileLog(String userCode,
-                              String method, String fileId,String logDetail){
+                              String method, String fileId, String logDetail) {
         OperationLogCenter.log(OperationLog.create().operation("FileServerLog").user(userCode)
-        .method(method).tag(fileId).time(DatetimeOpt.currentUtilDate()).content(logDetail));
+            .method(method).tag(fileId).time(DatetimeOpt.currentUtilDate()).content(logDetail));
     }
+
     @ApiOperation(
         value = "查询文件日志列表",
         notes = "request参数：optTag(文件id),userCode(操作人员)"
     )
     @GetMapping
     @WrapUpResponseBody
-    public List<? extends OperationLog> listFileLog( PageDesc pageDesc, HttpServletRequest request) {
+    public List<? extends OperationLog> listFileLog(PageDesc pageDesc, HttpServletRequest request) {
         Map<String, Object> searchColumn = BaseController.collectRequestParameters(request);
-        return this.optLogManager.listOptLog("FileServerLog", searchColumn, pageDesc.getRowStart(),pageDesc.getPageSize());
+        return this.optLogManager.listOptLog("FileServerLog", searchColumn, pageDesc.getRowStart(), pageDesc.getPageSize());
     }
 
 }
