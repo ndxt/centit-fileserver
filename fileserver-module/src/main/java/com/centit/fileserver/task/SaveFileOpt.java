414a9a62456eb7aff552ce1496119035398baa3c
zhf <zhf@centit.com>
2020-09-07 09:49:35 +0800

    修改

--- fileserver-module/src/main/java/com/centit/fileserver/task/SaveFileOpt.java
+++ fileserver-module/src/main/java/com/centit/fileserver/task/SaveFileOpt.java
@@ -2,6 +2,9 @@ package com.centit.fileserver.task;
 
 import com.centit.fileserver.common.FileOptTaskInfo;
 import com.centit.fileserver.utils.SystemTempFileUtils;
+import com.centit.framework.components.OperationLogCenter;
+import com.centit.framework.model.basedata.OperationLog;
+import com.centit.support.algorithm.DatetimeOpt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -23,5 +26,7 @@ public class SaveFileOpt extends FileOpt implements Consumer<FileOptTaskInfo> {
         String tempFilePath = SystemTempFileUtils.getTempFilePath(fileMd5, fileSize);
         save(tempFilePath, fileMd5, fileSize);
         logger.info("存储文件完成");
+        OperationLogCenter.log(OperationLog.create().operation("FileServerLog").user("admin")
+            .method("存储文件完成").tag(fileMd5).time(DatetimeOpt.currentUtilDate()).content(tempFilePath));
     }
 }
