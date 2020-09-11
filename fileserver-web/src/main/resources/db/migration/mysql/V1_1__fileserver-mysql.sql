5d4644beeb07c63e25484f1a163fe2826b8114c0
zhf <zhf@centit.com>
2020-09-11 08:56:57 +0800

    修改脚本

--- fileserver-web/src/main/resources/db/migration/mysql/V1_1__fileserver-mysql.sql
+++ fileserver-web/src/main/resources/db/migration/mysql/V1_1__fileserver-mysql.sql
@@ -87,6 +87,7 @@ create table file_info
    parent_folder            varchar(32) default '-1' comment '所属文件夹id',
    library_id           varchar(32) comment '库id',
    os_id varchar(32),
+   attached_file_md5 varchar(32),
    primary key (file_id)
 );
 
