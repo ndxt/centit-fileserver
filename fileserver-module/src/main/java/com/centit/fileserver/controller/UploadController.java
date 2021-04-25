b7a7d9e97a4a5caed9deb127ab55385991543eea
zhou_c <zhou_c@centit.com>
2021-04-25 09:49:26 +0800

    修改日志写入方式

--- fileserver-module/src/main/java/com/centit/fileserver/controller/UploadController.java
+++ fileserver-module/src/main/java/com/centit/fileserver/controller/UploadController.java
@@ -417,7 +417,6 @@ public class UploadController extends BaseController {
                 JSONObject json = UploadDownloadUtils.makeRangeUploadJson(uploadSize, token, token+"_"+size);
                 JsonResultUtils.writeOriginalJson(json.toString(), response);
             }
-            System.out.println("断点上传接口，日志入库开始");
             OperationLogCenter.log(OperationLog.create().operation(FileLogController.LOG_OPERATION_NAME)
                 .unit(formData.getLeft().getLibraryId())
                 .user(WebOptUtils.getCurrentUserCode(request))
@@ -425,7 +424,6 @@ public class UploadController extends BaseController {
                 .time(DatetimeOpt.currentUtilDate())
                 .content(formData.getLeft().getFileName())
                 .newObject(formData.getLeft()));
-            System.out.println("断点上传接口，日志入库结束");
         }catch (ObjectException e){
             logger.error(e.getMessage(),e);
             JsonResultUtils.writeHttpErrorMessage(e.getExceptionCode(),
