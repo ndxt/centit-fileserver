138a5457dc610a832f64a0b09dc7f95ce9731cbf
zhou_c <zhou_c@centit.com>
2021-10-21 16:30:27 +0800

    统一配置

--- fileserver-dubbo-server-config/src/main/java/com/centit/framework/dubbo/config/FileServerDubboConfig.java
+++ fileserver-dubbo-server-config/src/main/java/com/centit/framework/dubbo/config/FileServerDubboConfig.java
@@ -1,8 +1,10 @@
 package com.centit.framework.dubbo.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource({"classpath:dubbo-fileserver-server.xml"})
 public class FileServerDubboConfig {
+
 }
