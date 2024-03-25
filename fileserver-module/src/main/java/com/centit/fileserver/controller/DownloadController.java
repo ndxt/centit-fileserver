845425531e598422290d5eb5dfa2d7c21901f86c
codefan <codefan@centit.com>
2024-03-25 09:01:22 +0800

    添加必要的bean

--- fileserver-module/src/main/java/com/centit/fileserver/controller/DownloadController.java
+++ fileserver-module/src/main/java/com/centit/fileserver/controller/DownloadController.java
@@ -199,7 +199,7 @@ public class DownloadController extends BaseController {
         if(GeneralAlgorithm.isEmpty(fileInfo)){
             throw new ObjectException(ObjectException.BLANK_EXCEPTION, fileId+"无此文件信息！");
         }
-        if(StringBaseOpt.isNvl(fileInfo.getFileMd5())){
+        if(StringUtils.isBlank(fileInfo.getFileMd5())){
             throw new ObjectException(ObjectException.BLANK_EXCEPTION, fileId+"无此文件md5！");
         }
         FileStoreInfo fileStoreInfo = fileStoreInfoManager.getObjectById(fileInfo.getFileMd5());
