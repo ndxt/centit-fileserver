c8e880a84754ac3f94540fb4c7a76d6a0870e5b5
codefan <codefan@centit.com>
2024-05-22 17:32:06 +0800

    添加临时接口，创建索引

--- fileserver-module/src/main/java/com/centit/fileserver/task/DocumentIndexOpt.java
+++ fileserver-module/src/main/java/com/centit/fileserver/task/DocumentIndexOpt.java
@@ -23,7 +23,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Map;
 
 /**
