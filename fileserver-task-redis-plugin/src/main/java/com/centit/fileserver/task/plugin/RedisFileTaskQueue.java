461b9810300b48b4407966e073736caf78ba3223
codefan <codefan@centit.com>
2024-06-12 20:21:32 +0800

    修改redisclient

--- fileserver-task-redis-plugin/src/main/java/com/centit/fileserver/task/plugin/RedisFileTaskQueue.java
+++ fileserver-task-redis-plugin/src/main/java/com/centit/fileserver/task/plugin/RedisFileTaskQueue.java
@@ -1,8 +1,12 @@
 package com.centit.fileserver.task.plugin;
 
+import com.alibaba.fastjson2.JSON;
 import com.centit.fileserver.common.FileTaskInfo;
 import com.centit.fileserver.common.FileTaskQueue;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +28,10 @@ public class RedisFileTaskQueue implements FileTaskQueue {
 
     @Override
     public boolean add(FileTaskInfo taskInfo) {
-        //redisClient.
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisCommands<String, String> commands = connection.sync();
+        commands.rpush(FILE_TASK_INFO_KEY, JSON.toJSONString(taskInfo));
+        connection.close();
         //redisTemplate.opsForList().rightPush(FILE_TASK_INFO_KEY, taskInfo);
         logger.info("任务加入Redis队列");
         return true;
@@ -32,7 +39,13 @@ public class RedisFileTaskQueue implements FileTaskQueue {
 
     @Override
     public FileTaskInfo get() {
-        return null;
         //return (FileTaskInfo) redisTemplate.opsForList().leftPop(FILE_TASK_INFO_KEY);
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisCommands<String, String> commands = connection.sync();
+        String sjson = commands.lpop(FILE_TASK_INFO_KEY);
+        connection.close();
+        if(StringUtils.isBlank(sjson))
+            return null;
+        return JSON.parseObject(sjson, FileTaskInfo.class);
     }
 }
